200ededc0c5d904a2e094b2c97bb515241a5226b
Billjoy <billjoy.kilai@gmail.com>
2024-07-23 10:40:30 +0300

    laravel/vue.js

---
+++ src/routes/index.js
@@ -0,0 +1,55 @@
+import {createRouter, createWebHistory} from 'vue-router'
+
+import Homepage from "./../components/Homepage.vue"
+import Products from "./../components/Products.vue"
+import AboutUs from "./../components/AboutUs.vue"
+import ContactUs from "./../components/ContactUs.vue"
+import Cart from "./../components/Cart.vue"
+import ServicesOffered from "./../components/ServicesOffered.vue"
+import ManageProducts from "./../components/ManageProducts.vue"
+
+
+
+const routes = [
+    {
+      path: '/',
+      name: 'home',
+      component: Homepage,
+    },
+    {
+        path: '/products',
+        name: 'products',
+        component: Products,
+      },
+      {
+        path: '/aboutus',
+        name: 'aboutus',
+        component: AboutUs,
+      },
+      {
+        path: '/contactus',
+        name: 'contactus',
+        component: ContactUs,
+      },
+      {
+        path: '/cart',
+        name: 'cart',
+        component: Cart,
+      },
+      {
+        path: '/services',
+        name: 'services',
+        component: ServicesOffered,
+      },
+      {
+        path: '/manage_products',
+        name: 'manage_products',
+        component: ManageProducts,
+      },
+]
+const router = createRouter({
+  history: createWebHistory(),
+  routes, 
+})
+
+export default router
